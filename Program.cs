165795c39c78e08fb8e09ac3aeb0121d1f334a7e
Your Name <you@example.com>
2022-02-09 10:14:52 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 6;
+            if (number % 2 == 0)
+                Console.WriteLine("cutdur");
+            else
+                Console.WriteLine( "tekdir" );
+        }
+    }
+}
+        
+
+
+
+
